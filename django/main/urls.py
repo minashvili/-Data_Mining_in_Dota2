0cd0bc3c22cac4148f102774962f2e861c860679
minashvili <georgefergus473@gmail.com>
2023-07-22 20:35:28 +0300

    Занимался добавлением статических файлов и Bootstrap

--- django/main/urls.py
+++ django/main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index),
-    path('test', views.test)
+    path('', views.index, name='home'),
+    path('test', views.test, name='test')
 ]
+
+
