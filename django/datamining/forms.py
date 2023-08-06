363b994d99a22e14b521b7ee490aa691b3f80257
minashvili <georgefergus473@gmail.com>
2023-08-06 17:55:24 +0300

    Занимался Формой для добавления записей в БД Часть 2

--- django/datamining/forms.py
+++ django/datamining/forms.py
@@ -1,4 +1,30 @@
 
-#создать модель данных для ввода из формы
+from django import forms
+from .models import TableForForm
+from django.forms import ModelForm, TextInput
+
+class TableForFormForm(ModelForm):
+    class Meta:
+        model = TableForForm
+        fields = ['name', 'hero_name']
+        widgets = {
+            "name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Название Предмета',
+            }),
+            "hero_name": TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Имя Героя',
+            }),
+        }
+
+
+
+
+
+
+
+
+
 
 
--- django/datamining/views.py
+++ django/datamining/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ServisesInSupport
+from .models import TableForForm
+from .forms import TableForFormForm
+
 # Create your views here.
 def datamining_home(request):
     all_servises = ServisesInSupport.objects.order_by('-appearance_time')
     return render(request, 'datamining/datamining_home.html', {'all_servises': all_servises})
 
 def simple_form(request):
-    return render(request, 'datamining/simple_form.html')
+    error = ''
+    if request.method == 'POST':
+        form = TableForFormForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была не верной'
+
+    form = TableForFormForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'datamining/simple_form.html', data)
 
 
